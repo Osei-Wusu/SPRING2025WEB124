e8880c883fdcd76efd9396c1192caf28bbfb4474
Dominic <dappiah@stumail.jccc.edu>
2025-03-26 16:36:40 -0500

    Chapter 3 project in progress

---
+++ Chapter3Project/project2.js
@@ -0,0 +1,25 @@
+// Author: Dominic Appiah
+// Date: March 6, 2025
+// Description: CS 124 JavaScript file for Project 3 project 
+
+var fullName = "Dominic Appiah";
+console.log("Full Name: ", fullName);
+
+var desiredSalary = 80000;
+console.log("Desired Annual Salary: ", desiredSalary);
+
+var isVeteran = false; 
+console.log("Veteran Status: ", isVeteran);
+
+var friends = ["Dominic", "Osei", "Wusu"];
+console.log("Friends: ", friends);
+
+var friendsSalaries = [75000, 90000, 85000];
+console.log("Friends' Desired Salaries: ", friendsSalaries);
+
+var newFriend = {
+    firstName: "Destiny",
+    lastName: "Smith",
+    desiredSalary: 95000
+};
+console.log("Another Friend's Info: ", newFriend);
